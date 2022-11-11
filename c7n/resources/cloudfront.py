6e8452f8feced47266a85e2f9abb2d7d5c60c7ae
Kayden Ahn <kayden_ahn@intuit.com>
2022-11-11 02:49:01 -0800

    Cover WAfv1 use cases

--- c7n/resources/cloudfront.py
+++ c7n/resources/cloudfront.py
@@ -213,12 +213,12 @@ class IsWafV2Enabled(Filter):
             if state:
                 if not target_acl and r_web_acl_id in target_acl_ids:
                     results.append(r)
-                elif target_acl and r_web_acl_id in target_acl_ids:
+                elif r_web_acl_id in target_acl_ids:
                     results.append(r)
             else:
                 if not target_acl and r_web_acl_id not in target_acl_ids:
                     results.append(r)
-                elif target_acl and r_web_acl_id not in target_acl_ids:
+                elif r_web_acl_id not in target_acl_ids:
                     results.append(r)
         return results
 
